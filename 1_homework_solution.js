dd7f3d22ae2db99439c6025c0ae604e1b0f5e24b
MirasGitHub <121196122+MirasGitHub@users.noreply.github.com>
2023-03-29 15:06:30 +0400

    Update 1_homework_solution.js

--- 1_homework_solution.js
+++ 1_homework_solution.js
@@ -61,3 +61,39 @@ function Person(name, surname, age, gender, cars = []){
     };
 
 }
+
+let daniel916 = new Person("Daniel", "Barbakadze", 21, "M", []);
+let ilona = new Person("Elon", "Musk", 30, "M");
+let duti_picoti = new Car("BMW", "525", "1999");
+let stodevianosto = new Car("Mercedes", "E190", 1991);
+
+daniel916.buysCar(duti_picoti); // adds passed car
+daniel916.buysCar(stodevianosto); // adds passed car
+daniel916.sellsCar(duti_picoti); // removes passed car
+ilona.buysCar(stodevianosto); // adds passed car
+ilona.buysCar(duti_picoti); // adds passed car
+
+console.log({
+    daniel: {
+      fullName: daniel916.fullName(),
+      countCars: daniel916.countCars(),
+      getAllCarsInfo: daniel916.getAllCarsInfo(),
+    },
+    elon: {
+      fullName: ilona.fullName(),
+      countCars: ilona.countCars(),
+      getAllCarsInfo: ilona.getAllCarsInfo(),
+    },
+    duti_picoti: {
+      getOwnersCount: duti_picoti.getOwnersCount(),
+      getOwnerNames: duti_picoti.getOwnerNames(),
+      getFullInfo: duti_picoti.getFullInfo(),
+      getCarInfo: duti_picoti.getCarInfo(),
+    },
+    stodevianosto: {
+      getOwnersCount: stodevianosto.getOwnersCount(),
+      getOwnerNames: stodevianosto.getOwnerNames(),
+      getFullInfo: stodevianosto.getFullInfo(),
+      getCarInfo: stodevianosto.getCarInfo(),
+    },
+  });
